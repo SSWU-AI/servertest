45c6ab5a09e9d4d9e20affdc82c21fc038196298
kseohyuz <seohyun518612@naver.com>
2025-05-15 12:25:21 +0900

    Feat: 회원가입 구현

--- servertest/urls.py
+++ servertest/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from chatbot.views import chat_bot
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("chat/", chat_bot),
+    path('api/', include('chatbot.api.urls'))
 ]
